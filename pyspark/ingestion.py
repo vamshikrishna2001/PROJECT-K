5ff321415db4f923c7dd3b46ed288fa496c9238c
vamshikrishna2001 <vamshitargaryenstark@gmail.com>
2024-07-18 14:45:35 +0000

    sd

--- pyspark/ingestion.py
+++ pyspark/ingestion.py
@@ -23,6 +23,7 @@ df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:9093") \
     .option("subscribe", "sample_topic") \
+    .option("failOnDataLoss", "false") \
     .option("startingOffsets", "earliest") \
     .option("kafkaConsumer.pollTimeoutMs", "120000") \
     .load()
--- pyspark/submit.sh
+++ pyspark/submit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 kafka_consumer.py 
+python3 ingestion.py 
