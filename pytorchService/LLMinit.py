831022c5b77cc89e39dc337603e51124bd3e2f10
vamshikrishna2001 <vamshitargaryenstark@gmail.com>
2024-07-21 15:11:14 +0000

    Copied pytorch folder from master branch

--- pytorchService/LLMinit.py
+++ pytorchService/LLMinit.py
@@ -1,7 +1,11 @@
-from transformers import BertTokenizer, BertForSequenceClassification
-import torch
 
 
 # Initialize the tokenizer and model
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
+PretrainedModel = 'SamLowe/roberta-base-go_emotions'
+
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+tokenizer = AutoTokenizer.from_pretrained(PretrainedModel)
+model = AutoModelForSequenceClassification.from_pretrained(PretrainedModel)
+
